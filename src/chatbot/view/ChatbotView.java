c8f3568f44b6da08c59f65535948584a3eb59a82
Traitortots <jker3169@740s-pl-122.local>
2015-11-05 08:49:38 -0700

    added ICONS  to the popup windows

--- src/chatbot/view/ChatbotView.java
+++ src/chatbot/view/ChatbotView.java
@@ -1,24 +1,36 @@
 package chatbot.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
+/**
+ * popup display class for GUI interatcion in the chatbot project
+ * @author jker3169
+ *@version 1.3 11/5/15 Added Icons to popup windows!
+ */
 public class ChatbotView 
 {
 	
+	private String windMessage;
+	private ImageIcon chatIcon;
+	
 	public ChatbotView()
 	{
-		
+		windMessage = "Hello from your friendly chatbot :D";
+		chatIcon = new ImageIcon(getClass().getResource("images/AppIcon.png"));
 	}
 	
 	public void displayMessage(String message)
 	{
-		JOptionPane.showMessageDialog(null, message);
+		JOptionPane.showMessageDialog(null, message,windMessage, JOptionPane.PLAIN_MESSAGE, chatIcon);
 	}
 	
 	public String getResponce(String message)
 	{
 		String output = "";
-		output = JOptionPane.showInputDialog(null,message);
+		
+		output = JOptionPane.showInputDialog(null, message, windMessage, JOptionPane.PLAIN_MESSAGE, chatIcon, null, "Answer here :)").toString();
+		
 		return output;
 	}
 	
