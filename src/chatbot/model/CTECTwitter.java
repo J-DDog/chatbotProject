f06693cbbb6e5b745ac528c8331eb51dbaf70a88
Traitortots <jker3169@740s-pl-844.local>
2016-03-07 09:01:58 -0700

    started adding variables and comments to my twitter Class

--- src/chatbot/model/CTECTwitter.java
+++ src/chatbot/model/CTECTwitter.java
@@ -1,6 +1,26 @@
 package chatbot.model;
 
+import java.util.ArrayList;
+
+import twitter4j.*;
+
+/**
+ * @author jker3169
+ * @version 0.2
+ * 
+ * this is the twitter interaction class
+ * in this class I use what i've learned about api's to
+ * 
+ */
 public class CTECTwitter
 {
+	private ArrayList<Status> statusList;
+	private ArrayList<String> wordList;
+	
+	public CTECTwitter()
+	{
+		statusList = new ArrayList<Status>();
+		wordList = new ArrayList<String>();
+	}
 	
 }
