72c8d15610a4d912a23055c0fbb0dc3f1a178b6b
Traitortots <jker3169@740s-pl-844.local>
2016-03-09 09:23:41 -0700

    Added a new method based on state machins drives around the room randomly

--- src/chatbot/model/CTECTwitter.java
+++ src/chatbot/model/CTECTwitter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import chatbot.controller.ChatbotController;
 import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * @author jker3169
@@ -15,26 +16,29 @@ import twitter4j.*;
  */
 public class CTECTwitter
 {
-	private ChatbotController baseController;
+	private ChatbotController baseController;	
 	private Twitter chatbotTwitter;
 	
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
 	
 	public CTECTwitter(ChatbotController baseController)
-	{
+	{		
 		this.baseController = baseController;
 		chatbotTwitter = TwitterFactory.getSingleton();
 		
 		statusList = new ArrayList<Status>();
 		wordList = new ArrayList<String>();
+		
+		
+		
 	}
 	
 	public void sendTweet(String tweet)
 	{
 		try
 		{
-			chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program!"
+			chatbotTwitter.updateStatus("I, Jared, just tweeted from my Java Chatbot program!"
 					+ " #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
 		}
 		catch (TwitterException error)
